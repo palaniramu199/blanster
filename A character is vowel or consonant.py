eeddfb2d017d9b94c9fc6fedc2ef0181e68f91e1
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-01-10 21:01:51 +0530

    Update and rename A character is vowel or consonant to A character is vowel or consonant.py

--- A character is vowel or consonant.py
+++ A character is vowel or consonant.py
@@ -3,7 +3,7 @@
 int main()
 {
 char s;
-printf("enter the vowels :%c",&s);
+printf("%c",&s);
 scanf("%c",&s);
 if( s=='a'|| s=='e' || s=='i' || s=='o' || s=='u' || s=='A' || s=='E' || s=='I' || s=='O' || s=='U')
 {
