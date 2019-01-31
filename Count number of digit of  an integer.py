f0e5b29beae9e6fa32ea1303c9d4bdf0122ff2f7
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-01-31 21:44:33 +0530

    Update Count number of digit of  an integer.py

--- Count number of digit of  an integer.py
+++ Count number of digit of  an integer.py
@@ -1,2 +1,11 @@
-a=5,2,1
-print(len(a))
+def main():
+    count = 0
+    number = int(input(""))
+    while (number > 0):
+      number = number//10
+      count = count + 1
+    print (count)
+
+if __name__ == '__main__':
+    main()
+
