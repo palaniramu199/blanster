813c3136d0585c2891dc2c2fa616785710c8afa9
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-01-31 22:04:29 +0530

    Update display odd number between 2 intervals.py

--- display odd number between 2 intervals.py
+++ display odd number between 2 intervals.py
@@ -1,6 +1,8 @@
-a=1
-b=6
-for i in range(a+1,b):
-	if i%2==1:
-		print(i,end='\t')
-		
+def main():
+  a=1
+  b=6
+  for value in range(a+1,b+1):
+    if(value%2!=0):
+        print(value)
+if __name__ == '__main__':
+    main()
