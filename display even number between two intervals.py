79deca4608578e4fe2700f84c0d7d18063ebfd47
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-01-31 21:54:45 +0530

    Update display even number between two intervals.py

--- display even number between two intervals.py
+++ display even number between two intervals.py
@@ -1,5 +1,8 @@
-a=6
-b=9
-for i in range(a+1,b-1):
-	if i%2==0:
-		print(i,end='\t')
+def main():
+  m=int(input(""))
+  n=int(input(""))
+  for i in range(m+1,n+1):
+    if(i%2==0):
+        print(i)
+if __name__ == '__main__':
+    main()
