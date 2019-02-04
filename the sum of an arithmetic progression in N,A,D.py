50a19d0adaac926ac5fc5bc8fdcf266577ae98f8
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-02-04 14:25:40 +0530

    Update the sum of an arithmetic progression in N,A,D.py

--- the sum of an arithmetic progression in N,A,D.py
+++ the sum of an arithmetic progression in N,A,D.py
@@ -1,9 +1,10 @@
-N,A,D=input().split()
-N,A,D=int(N),int(A),int(D)
+#N,A,D=input().split()
+#N,A,D=int(N),int(A),int(D)
+N,A,D=5,1,0
 value=0
 total=0
-while N>=0:
+while A>=0:
     total+=value
-    value+=D
-    N-=1
+    value+=N
+    A-=1
 print(total)
