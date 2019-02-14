052acdbac7b25d9d6bfcac9588fd77f135ef4964
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-02-14 11:29:00 +0530

    Update 72.check vowels or not.py

--- 72.check vowels or not.py
+++ 72.check vowels or not.py
@@ -1,7 +1,11 @@
 import sys, string
 n=input()
+b=0
 for c in n:
 	if c =='a' or c=='i' or c=='e' or c=='o' or c=='u' or c =='A' or c=='I' or c=='E' or c=='O' or c=='U':
-		print("yes")
-	else:
-		print("no")
+		b=1
+		break
+if b:
+	print("yes")
+else:
+	print("no")
