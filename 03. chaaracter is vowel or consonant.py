ad38504c90af2025e8a3c873db55db999e713462
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-02-15 22:20:58 +0530

    Update and rename 3. chaaracter is vowel or consonant.py to 03. chaaracter is vowel or consonant.py

--- 03. chaaracter is vowel or consonant.py
+++ 03. chaaracter is vowel or consonant.py
@@ -1,4 +1,4 @@
-s='e'
+s=input()
 if( s=='a'or s=='e' or s=='i' or s=='o' or s=='u' or s=='A' or s=='E' or s=='I' or s=='O' or s=='U'):
 	print("Vowels")
 elif(s=='!'or s=='@'or s=='#'or s=='$' or s=='%' or s=='&'):
