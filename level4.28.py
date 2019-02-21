0ed46100420463b5cddfb0b217d3fbdb39cc58a7
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-02-21 10:40:42 +0530

    Create level4.28.py

---
+++ level4.28.py
@@ -0,0 +1,10 @@
+import sys, string, math
+n = int(input())
+L = [ int(x) for x in input().split()]
+L.sort()
+cnt = 0
+for i in range(0,n) :
+    #print(i+1, L[i], sum(L[:i]),L[i]-sum(L[:i]))
+    if L[i]-sum(L[:i]) >= 0 :
+        cnt += 1
+print(cnt)
