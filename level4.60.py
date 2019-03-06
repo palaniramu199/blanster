24c1e763f9492c0e44a60bde10a3cbe3d05a6acf
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-03-06 16:29:16 +0530

    Create level4.60.py

---
+++ level4.60.py
@@ -0,0 +1,14 @@
+import sys,string, math, itertools
+
+n = int(input())
+k = 3
+a = k
+while n > 0 :
+    if a == 0 :
+        k = 2*k
+        a = k
+    if n==1 :
+        print(a)
+        sys.exit()
+    n -= 1
+    a -= 1
