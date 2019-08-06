27ad2377aeff1eff9d357c2cbd3c24926c2fb339
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-08-06 18:39:57 +0530

    Create level3.14.py

---
+++ level3.14.py
@@ -0,0 +1,15 @@
+import sys,string,math,itertools
+a=input()
+l=list(itertools.permutations(a))
+b=[]
+for i in range(0,len(l)):
+    s=''.join(list(l[i]))
+    b.append(s)
+b.sort()
+c=[b[0]]
+for i in range(0,len(b)-1):
+    if b[i]!=b[i+1]:
+        c.append(b[i+1])
+for j in range(0,len(c)):
+print(c[j])
+    
