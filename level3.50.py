b5c5bd4df670fcde92aa14a13ac71167f5fbd250
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-08-08 19:03:05 +0530

    Update level3.50.py

--- level3.50.py
+++ level3.50.py
@@ -1,12 +1,12 @@
 a,b=map(int,input().split())
 if b>a:
     print(0)
-elif a==b:
+elif b==a:
     print(1)
 else:
     sum=0
     while a>=b:
         sum+=1
         a=a-b
-print(sum)
+    print(sum)
     
