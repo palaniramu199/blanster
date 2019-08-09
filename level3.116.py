ff005b4bc332c14b3e01167dfb2aedc6aec98131
palaniramu199 <46423555+palaniramu199@users.noreply.github.com>
2019-08-09 19:26:34 +0530

    Create level3.116.py

---
+++ level3.116.py
@@ -0,0 +1,17 @@
+#116
+a,b=input().split()
+a,b=list(a),list(b)
+c,d=len(a),len(b)
+print(c,d)
+e=max(c,d)
+f=min(c,d)
+g=e-f
+for i in range(1,g+1):
+    if c>d:
+        b.append(i)
+    else:
+        a.append(i)
+print(a)
+print(b)
+for i,j in zip(a,b):
+    print(i,j,end='',sep='')
